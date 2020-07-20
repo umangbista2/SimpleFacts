7f52fe718b5077acac6bf8d40c693686b30b7ba1
Umang Bista <umang.bista@jungelworks.com>
2020-07-20 19:56:10 +0530

    added facts view model and intialized the view controller

--- SimpleFacts/FactsViewController.swift
+++ SimpleFacts/FactsViewController.swift
@@ -10,12 +10,30 @@ import UIKit
 
 class FactsViewController: UIViewController {
 
+    private let viewModel: FactsViewModel!
+
     private let tableView = UITableView()
 
+    init(viewModel: FactsViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        super.loadView()
+        
+        view.backgroundColor = .white
+        
+        setupViews()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setupViews()
     }
 
 }
---
+++ SimpleFacts/FactsViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  FactsViewModel.swift
+//  SimpleFacts
+//
+//  Created by Umang Bista on 20/07/20.
+//  Copyright © 2020 Umang. All rights reserved.
+//
+
+import Foundation
+
+protocol FactsViewModel: class {
+    
+}
+
+
+final class FactsViewModelImplementation: FactsViewModel {
+    
+}
