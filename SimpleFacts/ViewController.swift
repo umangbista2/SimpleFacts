d18967d801e1308bbb8d5e97431ad52255c5fb3b
Umang Bista <umang.bista@jungelworks.com>
2020-07-20 19:34:08 +0530

    Initial Commit

---
+++ SimpleFacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleFacts
+//
+//  Created by Umang Bista on 20/07/20.
+//  Copyright © 2020 Umang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
